ee89096432cb2a3f1209d0ae8b7f0e3b6bb3d3c8
Win <winsdominoes@winscloud.net>
2026-03-10 15:11:21 +0000

    Inplemented a special case for cd - the current path changes!

--- src/bin/vssh.rs
+++ src/bin/vssh.rs
@@ -6,7 +6,7 @@
  * 
 */
 
-use std::{env, io::{Write, stdin, stdout}, process::Command};
+use std::{env, io::{Write, stdin, stdout}, path::Path, process::Command};
 
 fn main()
 {
@@ -27,14 +27,31 @@ fn main()
         let mut tokens = input.trim().split_whitespace();
         let cmd = tokens.next().unwrap();
         let args = tokens;
+        
+        // Match cmds
+        match cmd
+        {
+            // This is for if the cmd is `cd` which is special from other processes.
+            "cd" => 
+            {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root)
+                {
+                    eprintln!("{}", e);
+                }
+            },
+            cmd =>
+            {
+                // Creating child processes from the command
+                let mut child = Command::new(cmd)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
 
-        // Creating child processes from the command
-        let mut child = Command::new(cmd)
-            .args(args)
-            .spawn()
-            .unwrap();
-
-        // Wait for command to finish
-        child.wait();
+                // Wait for command to finish
+                child.wait();
+            }
+        }   
     }
 }
